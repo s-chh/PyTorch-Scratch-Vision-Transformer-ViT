f09b406508cf0ee07ce16843a8d7f8b8b801f826
s-chh <s.chh@outlook.com>
2024-03-10 11:03:35 -0700

    Add files via upload

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,5 @@
-python -u main.py --dataset mnist --epochs 100
-python -u main.py --dataset fmnist
-python -u main.py --dataset svhn --n_channels 3 --image_size 32
-python -u main.py --dataset cifar10 --n_channels 3 --image_size 32
+python main.py --dataset mnist --epochs 100
+python main.py --dataset fmnist
+python main.py --dataset svhn --n_channels 3 --image_size 32
+python main.py --dataset cifar10 --n_channels 3 --image_size 32
 
