2aafa11a98b0a6be5f06c7a892433a1d2a46677c
s-chh <sachin.chhabra@asu.edu>
2024-09-22 02:15:49 -0400

    Add CIFAR100

--- scripts.sh
+++ scripts.sh
@@ -2,4 +2,5 @@ python main.py --dataset mnist --epochs 100
 python main.py --dataset fashionmnist
 python main.py --dataset svhn --n_channels 3 --image_size 32 --embed_dim 128
 python main.py --dataset cifar10 --n_channels 3 --image_size 32 --embed_dim 128
+python main.py --dataset cifar100 --n_channels 3 --image_size 32 --embed_dim 128
 
